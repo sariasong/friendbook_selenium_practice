581a63717056150a36438dae8a22776d3e4c9d9f
Sarah Daenitz <sarahdaenitz@gmail.com>
2018-06-12 17:21:46 -0600

    added rspec testing

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Person, type: :model do
+
+  describe 'validations' do
+    context 'presense of validations' do
+      it { should validate_presence_of(:first_name) }
+      it { should validate_presence_of(:last_name) }
+    end
+  end
+end
